b7e0d855f478a3c5803df42fd78fed1b7f5be973
kaur0266 <kaur0266@algonquinlive.com>
2018-05-15 20:07:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/gurjinderkaur/androidlabs/ListItemsActivity.java
@@ -0,0 +1,13 @@
+package com.example.gurjinderkaur.androidlabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListItemsActivity extends Activity {
+//this is a comment
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_items);
+    }
+}
